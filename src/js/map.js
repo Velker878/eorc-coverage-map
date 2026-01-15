7813433c158b0bfbf554ae8cc3c8bc2fe352e245
Zachary Velker <zacharyvelker@gmail.com>
2026-01-15 08:33:50 -0500

    Add interactive UI panel with legend and layer toggles

--- src/js/map.js
+++ src/js/map.js
@@ -1,19 +1,32 @@
 import { loadCentreSectors } from "./centres.js";
 import { loadWards } from "./wards.js";
 
-const map = L.map("map").setView([45.4215, -75.6972], 11);
+const map = L.map("map").setView([45.394724, -75.474857], 11);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© OpenStreetMap contributors",
 }).addTo(map);
-
-(async () => {
-  const centreGeoJSON = await loadCentreSectors(map);
-  await loadWards(map, centreGeoJSON);
-})();
 
 map.createPane("sectorsPane");
 map.getPane("sectorsPane").style.zIndex = 400;
 
 map.createPane("wardsPane");
 map.getPane("wardsPane").style.zIndex = 500;
+
+let wardsLayer;
+
+(async () => {
+  const centreGeoJSON = await loadCentreSectors(map);
+
+  wardsLayer = await loadWards(map, centreGeoJSON);
+
+  const toggle = document.getElementById("toggle-wards");
+
+  toggle.addEventListener("change", () => {
+    if (toggle.checked) {
+      map.addLayer(wardsLayer);
+    } else {
+      map.removeLayer(wardsLayer);
+    }
+  });
+})();
